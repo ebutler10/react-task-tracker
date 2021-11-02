45e808ba33fb3100dcf0b4e12c23bc68ac7f1eb1
Emily Butler <emilybutler382@gmail.com>
2021-11-02 12:35:46 -0500

    delete tasks and message if notasks left

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Dec 5th',
-        reminder: true,
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Dec 5th",
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Zoom',
-        day: 'Dec 12th',
-        reminder: true,
+      id: 2,
+      text: "Zoom",
+      day: "Dec 12th",
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Trader Joe',
-        day: 'Dec 10th',
-        reminder: false,
+      id: 3,
+      text: "Trader Joe",
+      day: "Dec 10th",
+      reminder: false,
     },
-])
+  ]);
+
+  //delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
