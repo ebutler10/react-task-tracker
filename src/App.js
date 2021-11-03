1139de49d81924ab75d6d505ac2e6a9971aebe84
Emily Butler <emilybutler382@gmail.com>
2021-11-03 11:47:22 -0500

    half of reminder build

--- src/App.js
+++ src/App.js
@@ -25,16 +25,20 @@ const App = () => {
   ]);
 
   //delete task
-
-  const deleteTask = (id) => {
+   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map())
+  }
 
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show"
       )}
