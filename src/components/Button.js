ca274b6076dc19fc8cb09f13bc4c974f63192875
Emily Butler <emilybutler382@gmail.com>
2021-11-10 11:07:05 -0600

    button toggle bug fix an style added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
-const Button = ({ color, text, /*onClick*/ }) => {
+const Button = ({ color, text, onClick }) => {
   return (
-    <button /*onClick={onClick}*/ style={{ backgroundColor: color }} className="btn">
+    <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
       {text}
     </button>
   );
@@ -15,7 +15,7 @@ Button.defaultProps = {
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
-  //onClick: PropTypes.func,
+  onClick: PropTypes.func,
 };
 
 export default Button;
